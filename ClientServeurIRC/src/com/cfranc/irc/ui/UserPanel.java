f1e94bfd538b4521efb13b9187225ef1b84e3475
ericbenetti <eric.benetti@berger-levrault.fr>
2014-06-13 13:28:33 +0200

    US6 : Panel Utilisateur, + Look & Feeld Flamingo 6.2

---
+++ ClientServeurIRC/src/com/cfranc/irc/ui/UserPanel.java
@@ -0,0 +1,172 @@
+package com.cfranc.irc.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+import org.pushingpixels.substance.api.skin.BusinessBlueSteelSkin;
+import org.pushingpixels.substance.api.skin.SubstanceBusinessBlueSteelLookAndFeel;
+
+public class UserPanel extends JPanel {
+	
+	private JTextField lastNameField; 		//Nom
+	private JTextField firstNameField; 		//Prénom
+	private JTextField loginField;			//login/pseudo
+	private JPasswordField passwordField;	//password
+	private JTextField avatarField;			//Adresse de l'avatar
+	private JPanel avatarView;				//Panel de viisualisation de l'avatar
+	
+	public UserPanel() {
+		
+		Border border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		lastNameField = new JTextField("last name");
+		firstNameField = new JTextField("first name");
+		loginField= new JTextField("login");
+		passwordField = new JPasswordField("trustworthy");
+		avatarField = new JTextField("C:\\Users\\Administrateur\\Pictures\\couv.jpg");
+		avatarField.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				avatarView.repaint();	
+			}
+		});
+		
+		avatarView = new JPanel(new BorderLayout()) {
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				if ((new File(avatarField.getText())).exists()) {
+					g.drawImage( (new ImageIcon(avatarField.getText())).getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+				} else {
+					g.clearRect(0, 0, this.getWidth(), this.getHeight());
+				}
+			}
+		};
+		
+		avatarView.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2 ) {
+					JFileChooser fc = new JFileChooser();
+			        if (fc.showDialog(avatarField, "Select") == JFileChooser.APPROVE_OPTION) {
+			            File file = fc.getSelectedFile();
+			            avatarField.setText(file.getAbsolutePath());
+			            avatarView.repaint();
+			        }
+				}
+				
+			}
+		});
+		
+
+		JPanel pnlIdentity = new JPanel(new GridBagLayout());
+		pnlIdentity.setBorder(BorderFactory.createTitledBorder(border, "Information d'identification"));
+		pnlIdentity.add(new JLabel("Nom :", JLabel.RIGHT), new GridBagConstraints(0, 0, 1, 1, .0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlIdentity.add(lastNameField, new GridBagConstraints(1, 0, 1, 1, 1.0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlIdentity.add(new JLabel("Prénom :", JLabel.RIGHT), new GridBagConstraints(0, 1, 1, 1, .0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlIdentity.add(firstNameField, new GridBagConstraints(1, 1, 1, 1, 1.0, 0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0,0)); 
+
+		JPanel pnlLogin = new JPanel(new GridBagLayout());
+		pnlLogin.setBorder(BorderFactory.createTitledBorder(border, "Information de connexion"));
+		pnlLogin.add(new JLabel("Login / Pseudo :", JLabel.RIGHT), new GridBagConstraints(0, 0, 1, 1, .0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlLogin.add(loginField, new GridBagConstraints(1, 0, 1, 1, 1.0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlLogin.add(new JLabel("Mot de passe :", JLabel.RIGHT), new GridBagConstraints(0, 1, 1, 1, .0, .0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		pnlLogin.add(passwordField, new GridBagConstraints(1, 1, 1, 1, 1.0, 0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5,5), 0,0)); 
+		
+		JPanel pnlAvatar = new JPanel(new GridBagLayout());
+		pnlAvatar.setBorder(BorderFactory.createTitledBorder(border, "Avatar"));
+		pnlAvatar.add(avatarView, new GridBagConstraints(0, 0, 2, 1, 1., 1., GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5 ,5),0 ,0)); 
+		pnlAvatar.add(new JLabel("Fichier :", JLabel.RIGHT), new GridBagConstraints(0, 1, 1, 1, 0., .0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5 ,5), 0,0)); 
+		pnlAvatar.add(avatarField, new GridBagConstraints(1, 1, 1, 1, 1., .0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5 ,5), 0,0)); 
+		
+		this.setLayout(new GridBagLayout());
+		this.add(pnlIdentity, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.5, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(10,10,0,10), 0, 0));
+		this.add(pnlLogin, new GridBagConstraints(0, 1, 1, 1, .0, 0.5, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,10,10,10), 0, 0));
+		this.add(pnlAvatar, new GridBagConstraints(1, 0, 1, 2, .0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(10,0,10,10), 0, 0));
+	}
+	
+	public static void main(String[] args) {
+		//SubstanceLookAndFeel.setSkin(new GraphiteSkin());
+	    //SubstanceLookAndFeel.setSkin(new MarinerSkin());
+		SubstanceLookAndFeel.setSkin(new BusinessBlueSteelSkin());
+		try {
+			//UIManager.setLookAndFeel(new SubstanceGraphiteGlassLookAndFeel());
+			//UIManager.setLookAndFeel(new SubstanceMarinerLookAndFeel());
+			UIManager.setLookAndFeel(new SubstanceBusinessBlueSteelLookAndFeel());
+			
+		} catch (UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame("test panel");
+					frame.add (new UserPanel(), BorderLayout.CENTER);
+					frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					frame.pack();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public JTextField getLastNameField() {
+		return lastNameField;
+	}
+
+	public JTextField getFirstNameField() {
+		return firstNameField;
+	}
+
+	public JTextField getLoginField() {
+		return loginField;
+	}
+
+	public JPasswordField getPasswordField() {
+		return passwordField;
+	}
+
+	public JTextField getAvatarField() {
+		return avatarField;
+	}
+	
+	
+
+}
