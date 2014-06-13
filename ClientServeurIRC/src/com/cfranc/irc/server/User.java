c302d4f8ce6ea6ccf75d4a14cccddb75c8c5a2ef
XavierMalleville <misterx333@yahoo.fr>
2014-06-13 12:12:02 +0200

    User avec mise à jour de la base de donnée

--- ClientServeurIRC/src/com/cfranc/irc/server/User.java
+++ ClientServeurIRC/src/com/cfranc/irc/server/User.java
@@ -1,10 +1,24 @@
 package com.cfranc.irc.server;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.sqlite.JDBC;
+
 public class User {
 
 	private String name;
+	private String prenom;
 	private String login;
+	private String avatar;
 	private String pwd;
+	
+	static String urlBase;
 	
 	public String getLogin() {
 		return login;
@@ -20,14 +34,35 @@ public class User {
 		this.pwd = pwd;
 	}
 
-	public String getName() {
-	
+	public String getName() {	
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
 		
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	
+	public User(String name, String prenom ,String login, String avatar, String pwd) {
+		super();
+		this.name = name;
+		this.prenom= prenom;
+		this.login = login;
+		this.avatar= avatar;
+		this.pwd = pwd;
+	}
 	public User(String name, String login, String pwd) {
 		super();
 		this.name = name;
@@ -64,8 +99,128 @@ public class User {
 		return true;
 	}
 	
+	private boolean connectionVerifUser() {
+		//return  this.connectionVerifUser("c:"+ File.separator);
+		return  this.connectionVerifUser(urlBase);		
+	}
 	
-
+	private boolean connectionVerifUser(String filePath) {
+		boolean result = false;
+		if (!filePath.equals("")) {			
+			Connection connection = connectionBase ("jdbc:sqlite:" + filePath);
+			try {
+				// Action de vérification
+				result = controleExistanceUser(connection);
+			}
+			finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					System.err.println(e);
+				}
+			}			
+		}		
+		else {
+			System.out.println("Le chemin de la base est foireux");			
+		}	
+		return result;
+	}
+	private boolean controleExistanceUser(Connection connection) {
+		Statement statement;
+		boolean result = false;
+		try {
+			statement = connection.createStatement();
+			statement.setQueryTimeout(30);
+			result = statement.execute("SELECT * FROM TUserIRC WHERE Pseudo = " + this.login + " AND Pwd = " + this.pwd );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;						
+	}
 	
+	public boolean addUserBase () {
+		return  this.addUserBase(urlBase);
+	}
+	public boolean addUserBase (String filePath) {
+		boolean result = false;
+		if (!filePath.equals("")) {			
+			Connection connection = connectionBase ("jdbc:sqlite:" + filePath);
+			try {
+				if (! controleExistanceUser(connection)) {
+					Statement statement = connection.createStatement();
+					statement.setQueryTimeout(30);
+					statement.executeUpdate("INSERT INTO TUserIRC VALUES ("+this.name+","+this.prenom+","+this.login+","+this.avatar+","+this.pwd+") ");
+					result = true;
+				}	
+				else {
+					System.out.println("Erreur, l'utilisateur existe déjà");
+				}					
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					System.err.println(e);
+				}
+			}			
+		}		
+		else {
+			System.out.println("Le chemin de la base est foireux");			
+		}	
+		return result;			
+	}
+	public boolean delUserBase () {
+		return  this.addUserBase(urlBase);
+	}
+	public boolean delUserBase (String filePath) {
+		boolean result = false;
+		if (!filePath.equals("")) {			
+			Connection connection = connectionBase ("jdbc:sqlite:" + filePath);
+			try {
+				if (controleExistanceUser(connection)) {
+					Statement statement = connection.createStatement();
+					statement.setQueryTimeout(30);
+					statement.executeUpdate("DELETE * FROM TUserIRC WHERE Pseudo = " + this.login);
+					result = true;
+				}	
+				else {
+					System.out.println("Erreur, l'utilisateur n'existe plus");
+				}					
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					System.err.println(e);
+				}
+			}			
+		}		
+		else {
+			System.out.println("Le chemin de la base est foireux");			
+		}	
+		return result;	
+	}
 	
+	private Connection connectionBase(String url) {
+		Connection connection = null;			
+		if(JDBC.isValidURL(url)) {				
+			try {
+				connection = DriverManager.getConnection(url);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e.getMessage());
+			}
+		}
+		return connection;		
+	}	
 }
